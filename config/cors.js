698f7b4dc06c15d53658f18a82a2a0352188bf67
VidhulPrasad123 <vidhul@shade6.com>
2025-09-09 17:37:16 +0530

    updation main

--- config/cors.js
+++ config/cors.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 
 const corsOptions = {
-  origin: process.env.CORS_ORIGIN || '*', 
+  origin: process.env.CORS_ORIGIN , 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 };
