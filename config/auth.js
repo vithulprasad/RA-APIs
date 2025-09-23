db731e69a960adcb7995ad7ee1776e067fb77bc4
VidhulPrasad123 <vidhul@shade6.com>
2025-09-23 20:45:28 +0530

    updation authenticate

--- config/auth.js
+++ config/auth.js
@@ -1,24 +1,37 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const Admin_model = require("../models/Admin_model");
 
 const generateToken = (userId) => {
-  return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '30d' });
+  return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: "30d" });
 };
 
 const authenticate = (req, res, next) => {
-  const token = req.headers.authorization && req.headers.authorization.split(' ')[1];
+  const token =
+    req.headers.authorization && req.headers.authorization.split(" ")[1];
   if (!token) {
-    return res.status(403).json({ message: 'Unauthorized' });
+    return res.status(403).json({ message: "Unauthorized" });
   }
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err) {
-      if (err.name === 'TokenExpiredError') {
-        return res.status(403).json({ message: 'Token expired, please login again' });
+      if (err.name === "TokenExpiredError") {
+        return res
+          .status(403)
+          .json({ message: "Token expired, please login again" });
       }
-      return res.status(403).json({ message: 'Forbidden, please login' });
+      return res.status(403).json({ message: "Forbidden, please login" });
     }
     req.user = decoded;
     next();
   });
 };
+const check_admin = async (req, res, next) => {
+  const user = req.user;
+  const find_admin = await Admin_model.findOne({ _id: user });
+  if (!find_admin) {
+    res.status(404).json({ message: "access restricted you are not admin" });
+  } else {
+    next();
+  }
+};
 
-module.exports = { generateToken, authenticate };
+module.exports = { generateToken, authenticate,check_admin };
--- routes/ADMIN_route.js
+++ routes/ADMIN_route.js
@@ -7,7 +7,7 @@ const bannerController = require('../controllers/BANNER_controller')
 const orderController = require('../controllers/Order_controller')
 const home_controller = require("../controllers/home_controller")
 
-const { authenticate } = require('../config/auth');
+const { authenticate,check_admin } = require('../config/auth');
 
 router.post('/api/log_in', UserController.admin_login); 
 router.post('/api/sign_up', UserController.admin_sign_up); 
@@ -16,23 +16,23 @@ router.get('/api/verify_admin',UserController.verify);
 
 
 // category routes
-router.post('/api/create_category',authenticate, PC_controller.create_category); 
-router.get('/api/list_category',authenticate, PC_controller.list_category); 
-router.get('/api/list_category_main',authenticate, PC_controller.list_category_main); 
-router.get('/api/list_collection_main',authenticate, PC_controller.list_collections_main); 
-router.get('/api/category_single',authenticate,PC_controller.category_single)
-router.delete('/api/delete_category',authenticate, PC_controller.category_delete)
-router.post('/api/edit_category',authenticate, PC_controller.category_edit); 
+router.post('/api/create_category',authenticate,check_admin, PC_controller.create_category); 
+router.get('/api/list_category',authenticate,check_admin, PC_controller.list_category); 
+router.get('/api/list_category_main',authenticate,check_admin, PC_controller.list_category_main); 
+router.get('/api/list_collection_main',authenticate,check_admin, PC_controller.list_collections_main); 
+router.get('/api/category_single',authenticate,check_admin,PC_controller.category_single)
+router.delete('/api/delete_category',authenticate,check_admin, PC_controller.category_delete)
+router.post('/api/edit_category',authenticate,check_admin, PC_controller.category_edit); 
 
 // product routes
 
-router.post('/api/create_product',authenticate,PC_controller.product_create)
-router.post('/api/edit_product',authenticate,PC_controller.product_update)
-router.get('/api/get_product_by_id',authenticate,PC_controller.get_product_by_id)
-router.get('/api/product_list',authenticate,PC_controller.product_list) // pagination
-router.get('/api/product_delete',authenticate,PC_controller.product_delete) 
-router.get('/api/product_recover',authenticate,PC_controller.product_recover) 
-router.get('/api/search_product',authenticate,PC_controller.search_product)
+router.post('/api/create_product',authenticate,check_admin,PC_controller.product_create)
+router.post('/api/edit_product',authenticate,check_admin,PC_controller.product_update)
+router.get('/api/get_product_by_id',authenticate,check_admin,PC_controller.get_product_by_id)
+router.get('/api/product_list',authenticate,check_admin,PC_controller.product_list) // pagination
+router.get('/api/product_delete',authenticate,check_admin,PC_controller.product_delete) 
+router.get('/api/product_recover',authenticate,check_admin,PC_controller.product_recover) 
+router.get('/api/search_product',authenticate,check_admin,PC_controller.search_product)
 
 
 router.get('/product_dummy_inserter',PC_controller.product_dummy_insert)
@@ -40,48 +40,48 @@ router.get('/product_dummy_inserter',PC_controller.product_dummy_insert)
 
 
 //brand routes
-router.post('/api/create_brand',authenticate,Brand_controller.create_brand)
-router.post('/api/edit_brand',authenticate,Brand_controller.edit_brand)
-router.get('/api/single_brand',authenticate,Brand_controller.single_brand)
-router.get('/api/list_brands',authenticate,Brand_controller.list_brand)
-router.delete('/api/delete_brand',authenticate,Brand_controller.delete_brand)
-router.get('/api/brand_products',authenticate,Brand_controller.brand_products)
+router.post('/api/create_brand',authenticate,check_admin,Brand_controller.create_brand)
+router.post('/api/edit_brand',authenticate,check_admin,Brand_controller.edit_brand)
+router.get('/api/single_brand',authenticate,check_admin,Brand_controller.single_brand)
+router.get('/api/list_brands',authenticate,check_admin,Brand_controller.list_brand)
+router.delete('/api/delete_brand',authenticate,check_admin,Brand_controller.delete_brand)
+router.get('/api/brand_products',authenticate,check_admin,Brand_controller.brand_products)
 
 
 
 
 
 //banner routes
-router.post("/api/create_banner",authenticate, bannerController.createBanner);
-router.put("/api/edit_banner/:id",authenticate, bannerController.updateBanner);
-router.get("/api/single_banner/:id",authenticate, bannerController.getBannerById);
-router.get("/api/all_banners",authenticate, bannerController.getBanners);
-router.delete("/api/delete_banner/:id",authenticate, bannerController.deleteBanner);
+router.post("/api/create_banner",authenticate,check_admin, bannerController.createBanner);
+router.put("/api/edit_banner/:id",authenticate,check_admin, bannerController.updateBanner);
+router.get("/api/single_banner/:id",authenticate,check_admin, bannerController.getBannerById);
+router.get("/api/all_banners",authenticate,check_admin, bannerController.getBanners);
+router.delete("/api/delete_banner/:id",authenticate,check_admin, bannerController.deleteBanner);
 
 
 //orders
-router.get("/api/orders",authenticate, orderController.getAllOrders_admin);
-router.get("/api/search_orders", authenticate,orderController.search_orders);
-router.get("/api/get_order_admin_single",authenticate, orderController.get_order_admin_single);
-router.post("/api/order_action",authenticate, orderController.order_action);
-router.get("/api/get_customers",authenticate, orderController.get_customers);
-router.get("/api/get_customer_profile",authenticate, orderController.get_customer_profile);
-router.post("/api/send_customer_email",authenticate, orderController.send_customer_email);
+router.get("/api/orders",authenticate,check_admin, orderController.getAllOrders_admin);
+router.get("/api/search_orders", authenticate,check_admin,orderController.search_orders);
+router.get("/api/get_order_admin_single",authenticate,check_admin, orderController.get_order_admin_single);
+router.post("/api/order_action",authenticate,check_admin, orderController.order_action);
+router.get("/api/get_customers",authenticate,check_admin, orderController.get_customers);
+router.get("/api/get_customer_profile",authenticate,check_admin, orderController.get_customer_profile);
+router.post("/api/send_customer_email",authenticate,check_admin, orderController.send_customer_email);
 
 
 // common image uploader
-router.get('/api/search_banner',authenticate,home_controller.search_banner);
-router.get('/api/search_category',authenticate,home_controller.search_category);
-router.get('/api/search_product_for',authenticate,home_controller.search_product);
-router.post("/api/create_home_settings", authenticate,home_controller.create_home_settings);
-router.post("/api/create_collection_section",authenticate, home_controller.create_collection_section);
-router.get('/api/get_home_section',authenticate,home_controller.get_home_section);
-router.get('/api/get_collection_section',authenticate,home_controller.get_collection_section);
-router.get('/api/product_details',authenticate,home_controller.product_details);
-router.get('/api/review_action',authenticate,home_controller.review_action);
-router.get('/api/add_quantity',authenticate,home_controller.add_quantity);
+router.get('/api/search_banner',authenticate,check_admin,home_controller.search_banner);
+router.get('/api/search_category',authenticate,check_admin,home_controller.search_category);
+router.get('/api/search_product_for',authenticate,check_admin,home_controller.search_product);
+router.post("/api/create_home_settings", authenticate,check_admin,home_controller.create_home_settings);
+router.post("/api/create_collection_section",authenticate,check_admin, home_controller.create_collection_section);
+router.get('/api/get_home_section',authenticate,check_admin,home_controller.get_home_section);
+router.get('/api/get_collection_section',authenticate,check_admin,home_controller.get_collection_section);
+router.get('/api/product_details',authenticate,check_admin,home_controller.product_details);
+router.get('/api/review_action',authenticate,check_admin,home_controller.review_action);
+router.get('/api/add_quantity',authenticate,check_admin,home_controller.add_quantity);
 
-router.delete('/api/delete_product_collection',authenticate,home_controller.delete_product_collection);
+router.delete('/api/delete_product_collection',authenticate,check_admin,home_controller.delete_product_collection);
 
 
 
